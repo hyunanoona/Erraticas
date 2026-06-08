4aabf113e7d54961c431090c513b3e663caa33f6
Nicole ✮⋆˙ <folconicole1@gmail.com>
2026-06-07 21:14:14 -0300

    Creacion de Scripts "GameManager" y "MenuManager"
    Creación de la carpeta de scripts, y agregado de los script GameManager y MenuManager como base para empezar a trabajar con los respectivos códigos

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    /*
+    GameManager ᝰ
+    ಄ Se encarga de las funcionalidades básicas del juego (Menú, Pausa) y sus niveles (pasar de un nivel a otro, guardar las partidas).
+	಄ Conecta las animaciones y los audios con los eventos del juego. 
+    */
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
