e09b13d1fe21cc060a566893a879b429c30fd1af
Nicole ✮⋆˙ <folconicole1@gmail.com>
2026-06-09 17:47:33 -0300

    Boceto de MenuInicial hecho, conexión de botones
    En la escena del menú inicial se agregaron los botones y sus conexiones con el código para su funcionamiento

--- Assets/Scripts/MenuInicial.cs
+++ Assets/Scripts/MenuInicial.cs
@@ -1,13 +1,28 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 
 public class MenuInicial : MonoBehaviour
 {
+    [Header(". ݁₊ ⊹ . ݁ Referencias y Variables  ݁ . ⊹ ₊ ݁.")]
+    [SerializeField] private Button botonJugar;
+    [SerializeField] private Button botonSalir;
 
+    void Start()
+    {
+        if (botonJugar != null)
+        {
+            botonJugar.onClick.AddListener(Jugar);
+        }
+
+        if (botonSalir != null)
+        {
+            botonSalir.onClick.AddListener(Salir);
+        }
+    }
     public void Jugar()
     {
-        SceneManager.LoadScene("Nivel1");
+        SceneManager.LoadScene("NivelUno");
     }
 
     public void Salir()
