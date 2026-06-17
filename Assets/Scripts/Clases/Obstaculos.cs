778fa7926c1de706a823678ab09db4d3dff0846a
Florencia <florenciabelenfigueroa05@gmail.com>
2026-06-17 20:54:35 -0300

    Creacion del script "obstaculos"
    Se creo el script de "obstaculos" el cual contiene los hijos de la clase item

--- Assets/Scripts/Clases/Obstaculos.cs
+++ Assets/Scripts/Clases/Obstaculos.cs
@@ -2,17 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Obstaculos : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+/*
+    ╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮
+      Obstaculos ᝰ
+        Script que contiene las clases hijas de todos los obstacuos (veneno y ratonera)
+        Todos los obstaculos usan el TagPermitido => "Ambos" ya que afectan a cualquier raton.
+    ╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯
+*/
+
+//  •• <<───────────────────────≪•◦ Veneno ◦•≫───────────────────────>> ••
+
+public class Veneno : ClaseItem
     {
-        
+        public int danioVida = 10;
+        protected override string TagPermitido => "Ambos";
+        public Veneno() { nombreItem = "Veneno"; }
+        protected override void AplicarEfectoCazador(DatosCazador cazador)
+        {
+            //cazador.RestarVida(danioVida);
+        }
+        protected override void AplicarEfectoSoporte(DatosSoporte soporte)
+        {
+            //soporte.RestarVida(danioVida);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+//  •• <<──────────────────────≪•◦ Ratonera ◦•≫──────────────────────>> ••
+
+public class Ratonera : ClaseItem 
+{
+    public float tiempoInmovilizado = 5f;
+
+    public int puntosQueResta = 5;
+    protected override string TagPermitido => "Ambos";
+    public Ratonera() { nombreItem = "Ratonera"; }
+
+    protected override void AplicarEfectoCazador(DatosCazador cazador)
     {
-        
+        //Resta puntaje
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.SumarPuntos(-puntosQueResta);
+        }
+
+        //cazador.Inmovilizar(tiempoInmovilizado);
     }
+    protected override void AplicarEfectoSoporte(DatosSoporte soporte)
+    {
+        //Resta puntaje
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.SumarPuntos(-puntosQueResta);
+        }
+    }
+
+    //soporte.Inmovilizar(tiempoInmovilizado);
 }
