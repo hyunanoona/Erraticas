1d119a96d98c38a9d3200f937c7bd60bce6e46d3
Florencia <florenciabelenfigueroa05@gmail.com>
2026-06-22 15:34:56 -0300

    Ajustes en la camara, plataformas y spawn
    Se termino de pulir el movimiento de la camara sobre los jugadores en el nivel 2
    
    Se agrego el tag Pisable en todas las plataformas
    
    Se ajusto la linea de spawn de los items y sus colisiones para que puedan caer y quedarse sobre las plataformas y a su vez sean recolectados por los jugadores

---
+++ Assets/Scripts/Clases/Spawner/SueloDetector.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    ╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮
+      SueloDetector ᝰ
+        Se usa para detectar cuando el item toca el suelo y asi evitar que siga cayendo, tambien 
+        se usa para detectar cuando el jugador toca el item y asi destruirlo para simular que lo
+        recolecto
+    ╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯
+*/
+
+public class SueloDetector : MonoBehaviour
+{
+
+    //  •• <<────────────────≪•◦ Collision ◦•≫────────────────>> ••
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Esto mantiene el ítem quieto al tocar el suelo
+        if (collision.gameObject.CompareTag("Pisable"))
+        {
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.bodyType = RigidbodyType2D.Kinematic;
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Chquea si el jugador toca el item asi se destruye para simular que lo recolecto
+        if (other.CompareTag("Cazador") || other.CompareTag("Soporte"))
+        {
+            Destroy(gameObject); 
+        }
+    }
+}
