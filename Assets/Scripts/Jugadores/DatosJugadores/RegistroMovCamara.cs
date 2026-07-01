0408883d15d4dc5d83eabf0a38f8e430ca980a50
Florencia <florenciabelenfigueroa05@gmail.com>
2026-07-01 18:20:47 -0300

    Ajustes en los prefabs de obstaculos y script de "RegistroMovCamara"
    Se intercambiaron los assets de los prefabs de obstaculos y se corrigio el script que se encarga de agrupar a los personajes para que la camara los siga en grupo

--- Assets/Scripts/Jugadores/DatosJugadores/RegistroMovCamara.cs
+++ Assets/Scripts/Jugadores/DatosJugadores/RegistroMovCamara.cs
@@ -1,24 +1,25 @@
 using UnityEngine;
-// IMPORTANTE: Esta línea nos permite usar las herramientas de Cinemachine
 using Cinemachine;
+
+/*
+    ╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮
+      RegistroMovCamara
+        Este script se encarga de registrar el objeto al CinemachineTargetGroup para que la 
+        camara lo siga    
+    ╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯
+*/
 
 public class RegistroMovCamara : MonoBehaviour
 {
     void Start()
     {
-        // Buscamos el componente en la escena
+        // Se busca el componente en la escena
         CinemachineTargetGroup grupoCamara = FindFirstObjectByType<CinemachineTargetGroup>();
 
         if (grupoCamara != null)
         {
-            // SOLUCIÓN PARA VERSIÓN NUEVA: Usamos AddMember en lugar de AddTarget
+            // Se agrega el objeto al grupo de la camara
             grupoCamara.AddMember(this.transform, 1f, 0f);
-
-            Debug.Log($"¡{gameObject.name} se registró correctamente usando AddMember!");
-        }
-        else
-        {
-            Debug.LogWarning("No se encontró ningún CinemachineTargetGroup en la escena.");
         }
     }
 }
